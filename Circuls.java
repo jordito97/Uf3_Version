a7243d7ca498b69aa73b474c9ef32804ee75f06e
jordi <jordi.jd97@gmail.com>
2020-05-27 13:30:07 +0200

    Version Final

--- Circuls.java
+++ Circuls.java
@@ -18,7 +18,14 @@ public class Circuls {
 
         return radio;
     }
-  
+    public double perimetro(){
+
+        return (3.14*2*this.radio);
+
+    }
+    public double area(){
+        return (Math.pow(this.radio, 2)*3.14);
+    }
 
 
 
