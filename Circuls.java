bb0d75a7da6e2e20ce6b86919ccadd500e4f95da
jordi <jordi.jd97@gmail.com>
2020-05-27 13:28:38 +0200

    Version 1

---
+++ Circuls.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Circuls {
+    public double radio;//unica propiedad del objeto
+
+    public Circuls(double radio) {//constructor
+        super();
+        this.radio = radio;
+    }
+
+    public void setRadio(float radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+
+        return radio;
+    }
+  
+
+
+
+
+}
